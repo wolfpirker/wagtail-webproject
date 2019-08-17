6606d832fbfc625d9b60a1d88d7d03995f617b32
Wolfgang Pirker <w_pirker@gmx.de>
2019-08-17 23:16:49 +0200

    Adding a banner and banner fields, initial progress

--- home/models.py
+++ home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import RichTextField
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     """Home page model."""
@@ -11,9 +13,30 @@ class HomePage(Page):
 
     # fields cannot be blank in the form, but in database null is OK
     banner_title = models.CharField(max_length=100, blank=False, null=True)
-
+        # Note: models is specific to Django, django.db := models
+    banner_subtitle = RichTextField(features=["bold", "italic"]) # wagtail specific
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image", 
+        # classname of the image, see wagtail documentation
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    # cta: banner call to action, link to another page 
+    # link to another wagtail page we have not created yet
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta")
     ]
 
     class Meta:
