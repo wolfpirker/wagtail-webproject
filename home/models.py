771ee3bf36afd6f9e25b42a7ffda03d27aed5d1a
Wolfgang Pirker <w_pirker@gmx.de>
2019-08-18 21:32:01 +0200

    added standard page and contact form page functionality, broken

--- home/models.py
+++ home/models.py
@@ -10,6 +10,8 @@ from wagtail.admin.edit_handlers import (
     MultiFieldPanel,
     StreamFieldPanel
 )
+from wagtail.snippets.models import register_snippet
+
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
@@ -50,75 +52,98 @@ class HomePage(Page):
         PageChooserPanel("banner_cta")
     ]
 
-    #from bakerydemo
-    class StandardPage(Page):
-        """
-        A generic content page. On this demo site we use it for an about page but
-        it could be used for any type of page content that only needs a title,
-        image, introduction and body field
-        """
-
-        introduction = models.TextField(
-            help_text='Text to describe the page',
-            blank=True)
-        image = models.ForeignKey(
-            'wagtailimages.Image',
-            null=True,
-            blank=True,
-            on_delete=models.SET_NULL,
-            related_name='+',
-            help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
-        )
-        body = StreamField(
-            BaseStreamBlock(), verbose_name="Page body", blank=True
-        )
-        content_panels = Page.content_panels + [
-            FieldPanel('introduction', classname="full"),
-            StreamFieldPanel('body'),
-            ImageChooserPanel('image'),
-    ]
-
-    # from bakerydemo:
-    class FormField(AbstractFormField):
-        """
-        Wagtailforms is a module to introduce simple forms on a Wagtail site. It
-        isn't intended as a replacement to Django's form support but as a quick way
-        to generate a general purpose data-collection form or contact form
-        without having to write code. We use it on the site for a contact form. You
-        can read more about Wagtail forms at:
-        http://docs.wagtail.io/en/latest/reference/contrib/forms/index.html
-        """
-        page = ParentalKey('FormPage', related_name='form_fields', on_delete=models.CASCADE)
-
-    class FormPage(AbstractEmailForm):
-        image = models.ForeignKey(
-            'wagtailimages.Image',
-            null=True,
-            blank=True,
-            on_delete=models.SET_NULL,
-            related_name='+'
-        )
-        body = StreamField(BaseStreamBlock())
-        thank_you_text = RichTextField(blank=True)
-
-        # Note how we include the FormField object via an InlinePanel using the
-        # related_name value
-        content_panels = AbstractEmailForm.content_panels + [
-            ImageChooserPanel('image'),
-            StreamFieldPanel('body'),
-            InlinePanel('form_fields', label="Form fields"),
-            FieldPanel('thank_you_text', classname="full"),
-            MultiFieldPanel([
-                FieldRowPanel([
-                    FieldPanel('from_address', classname="col6"),
-                    FieldPanel('to_address', classname="col6"),
-                ]),
-                FieldPanel('subject'),
-            ], "Email"),
-        ]
-
-
     class Meta:
 
         verbose_name = "Home Page"
         verbose_name_plural = "Home Pages"
+
+#from bakerydemo
+@register_snippet
+class FooterText(models.Model):
+    """
+    This provides editable text for the site footer. Again it uses the decorator
+    `register_snippet` to allow it to be accessible via the admin. It is made
+    accessible on the template via a template tag defined in base/templatetags/
+    navigation_tags.py
+    """
+    body = RichTextField()
+
+    panels = [
+        FieldPanel('body'),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    class Meta:
+        verbose_name_plural = 'Footer Text'
+
+#from bakerydemo
+class StandardPage(Page):
+    """
+    A generic content page. On this demo site we use it for an about page but
+    it could be used for any type of page content that only needs a title,
+    image, introduction and body field
+    """
+
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+    body = StreamField(
+        BaseStreamBlock(), verbose_name="Page body", blank=True
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+        StreamFieldPanel('body'),
+        ImageChooserPanel('image'),
+]
+
+# from bakerydemo:
+class FormField(AbstractFormField):
+    """
+    Wagtailforms is a module to introduce simple forms on a Wagtail site. It
+    isn't intended as a replacement to Django's form support but as a quick way
+    to generate a general purpose data-collection form or contact form
+    without having to write code. We use it on the site for a contact form. You
+    can read more about Wagtail forms at:
+    http://docs.wagtail.io/en/latest/reference/contrib/forms/index.html
+    """
+    page = ParentalKey('FormPage', related_name='form_fields', on_delete=models.CASCADE)
+
+class FormPage(AbstractEmailForm):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = StreamField(BaseStreamBlock())
+    thank_you_text = RichTextField(blank=True)
+
+    # Note how we include the FormField object via an InlinePanel using the
+    # related_name value
+    content_panels = AbstractEmailForm.content_panels + [
+        ImageChooserPanel('image'),
+        StreamFieldPanel('body'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
+
+
+    
