2c4b19480a503a6f94956d42ed250823a191f44d
Wolfgang Pirker <w_pirker@gmx.de>
2019-08-25 20:34:16 +0200

    fix order of blog post, fix only show published posts

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,12 @@ from wagtail.search import index
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post date")
