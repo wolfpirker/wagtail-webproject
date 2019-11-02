a3d16ea92b786ce924e101ad16ee4379c3d7e50c
Wolfgang Pirker <w_pirker@gmx.de>
2019-11-02 23:03:47 +0100

    added template caching to blog, tours, and destination index pages

--- siemreap/settings/dev.py
+++ siemreap/settings/dev.py
@@ -31,18 +31,11 @@ from django.urls import path
 #    ] + urlpatterns
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/dbname',
-        # for django-redis < 3.8.0, use:
-        # 'LOCATION': '127.0.0.1:6379',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": "/home/wolf/siemreap-page/cache"
         }
-    }
 }
-
-
 
 #WAGTAILSEARCH_BACKENDS = {
 #    'default': {
