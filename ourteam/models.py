814ec2352e51ae16896a737acbdb824df3062843
Wolfgang Pirker <w_pirker@gmx.de>
2019-11-24 16:32:01 +0100

    improvements to contact forms on guide pages

--- ourteam/models.py
+++ ourteam/models.py
@@ -4,10 +4,10 @@ from django.db import models
 from django import forms
 
 # django contact form
-from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse # Add this
-from .forms import ContactForm
+#from django.shortcuts import render
+#from django.views.generic import View
+#from django.http import HttpResponse # Add this
+#from .forms import ContactForm
 
 from wagtail.core.models import Page, Orderable
 from wagtail.admin.edit_handlers import FieldPanel
@@ -29,6 +29,14 @@ from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
 from streams import blocks
 from django.utils.text import slugify
+
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
+from django.shortcuts import redirect
+from django.forms import widgets
 
 class GuideQualification(models.Model):
     """Guide qualification for a snippet"""
@@ -69,12 +77,21 @@ class GuideToursOrderable(Orderable):
     def __str__(self):
         return self.page.title
 
-class OurteamPage(Page):
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'OurteamPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+class OurteamPage(AbstractEmailForm):
     """our Team page"""
 
     template = "ourteam/ourteam_page.html"
 
     subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    thank_you_text = RichTextField(blank=True)
 
     content = StreamField(
         [
@@ -87,14 +104,67 @@ class OurteamPage(Page):
 
     )
 
-    # body = RichTextField(help_text='Add your main text, you can embed images')
+    #def get_context(self, request):
+    #    #if request.POST:
+    #    context = super().get_context(request)
+    #    guide = request.GET.get('guide')
+    #    if guide is not None:
+
+    _guide = None
+    @property
+    def guide_page(self):
+        return self._guide
+
+    @guide_page.setter
+    def guide_page(self, current_guide):
+        self._guide = current_guide
+
+    @property
+    def return_form(self):
+        form = self.get_form()
+        return form
+    
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        # iterate through the fields in the generated form
+        for name, field in form.fields.items():
+            # here we want to adjust the widgets on each field
+            # if the field is a TextArea - adjust the rows
+            if isinstance(field.widget, widgets.Input):
+                field.widget.attrs['placeholder'] = field.label
+                if field.label == 'Guide' and (self.guide_page != None): 
+                    field.widget.attrs['value'] = self.guide_page.title
+                #field.label = ''
+            if isinstance(field.widget, widgets.Textarea):
+                field.widget.attrs['placeholder'] = field.label
+                #field.label = ''                           
+            # for all fields, get any existing CSS classes and add 'form-control'
+            # ensure the 'class' attribute is a string of classes with spaces
+            css_classes = field.widget.attrs.get('class', '').split()
+            css_classes.append('form-control')
+            field.widget.attrs.update({'class': ' '.join(css_classes)})
+        return form
+
+    #def render_landing_page(self, request, form_submission=None, *args, **kwargs):
+    #    page = request.POST.get("guide")
+    #    url = self.url
+    #    return redirect(page, permanent=False)   
 
     search_fields = Page.search_fields + [
         index.SearchField('subtitle'),
     #    index.SearchField('body'),
     ]
 
-    content_panels = Page.content_panels + [
+    content_panels = AbstractEmailForm.content_panels + [
+        InlinePanel('form_fields', label='Form Fields'),
+            FieldPanel('thank_you_text'),
+            MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel("subject"),
+            ], heading="Email Settings"),
         FieldPanel("subtitle"),
         StreamFieldPanel("content")
     ]
@@ -125,7 +195,7 @@ class TourGuidePage(Page):
     hourly_rate_low_season = models.DecimalField(max_digits=4, decimal_places=2,help_text="hourly rate low season")
     hourly_rate_high_season = models.DecimalField(max_digits=4, decimal_places=2,help_text="hourly rate high season")
     additional_charge_per_tour = models.IntegerField(help_text="added charge per group for each tour")
-    form = ContactForm()
+    #form = ContactForm()
 
     main_province = models.ForeignKey(
         'tours.TourProvince',
@@ -135,22 +205,35 @@ class TourGuidePage(Page):
         related_name='+'
     )
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        parent = self.get_parent()
+        parent.specific.guide_page = self
+        form = parent.specific.return_form
+        context['ourteamsform'] = form
+        return context
+    #    context['parent'] = parent
+    #    return context
+    @property
+    def return_form(self):
+        return self.get_parent().specific.return_form()
+
     def clean(self):
         """Override the values of title and slug before saving."""
         super(TourGuidePage, self).clean()
         self.title = "%s %s" % (self.first_name, self.last_name)
         self.slug = slugify(self.title)    
 
-    def contact_us(self, request):
-        if request.method == 'POST':
-            form = ContactForm(request.POST)
-            if form.is_valid():
-                # send email code goes here
-                return HttpResponse('Thanks for contacting us!')
-        else:
-            form = ContactForm()
+    #def contact_us(self, request):
+    #    if request.method == 'POST':
+    #        form = ContactForm(request.POST)
+    #        if form.is_valid():
+    #            # send email code goes here
+    #            return HttpResponse('Thanks for contacting us!')
+    #    else:
+    #        form = ContactForm()
 
-        return render(request, 'contact-landing.html', {'form': form})
+    #    return render(request, 'contact-landing.html', {'form': form})
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
--- ourteam/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import View
-from django.http import HttpResponse # Add this
-
-from models import ContactForm
